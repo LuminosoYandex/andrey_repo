d363c309fe29c46d19841ca3ae57876accee009a
__AndreyDocker__ <andreydocker@yandex.ru>
2016-12-18 13:30:47 +0300

    function plus

--- test_edit.php
+++ test_edit.php
@@ -1,29 +1,6 @@
 <?php
-	class testEdit {
-		var $jpg;
-		var $txt;
-		
-		function text($text) {
-			echo $text;
-		}
-		function pict($text) {
-			echo $text;
-		}
+	function plus($x,$y){
+		return $x+$y;
 	}
-	if ($_SERVER['REQUEST_METHOD']=='POST'){
-		echo $_POST["comment"];
-	}
+	echo plus(4,3);
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	<form action="test_edit.php" method="post">
-		<textarea name="comment" cols="40" rows="3"></textarea>
-		<input type="submit" value="Отправить">
-	</form>
-</body>
-</html>
