016cacdc0f5eeeb41dfa3c23f7823c1ce73b93a2
__AndreyDocker__ <andreydocker@yandex.ru>
2016-12-18 12:37:02 +0300

    Initial commit

---
+++ andrey.php
@@ -0,0 +1,10 @@
+<?php
+echo "<html><head>"; 
+echo '<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">'; 
+echo "<title>  PHP </title>"; 
+echo "</head>"; 
+echo "<body>"; 
+echo "<h1>!  PHP !</h1>"; 
+echo "</body>"; 
+echo "</html>"; 
+?>
